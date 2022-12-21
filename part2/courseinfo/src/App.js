0fc75f54502ca8a3d10c1fb34a5baaaa9d9825ea
sunshinetainted <31520928+sunshinetainted@users.noreply.github.com>
2022-12-21 13:44:47 +0100

    Solution exercise 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -20,7 +20,7 @@ const Total = ({parts}) => {
     return s    
   }, 0)      
   return (
-    <p>Number of exercises {total}</p>
+    <p><strong>Total of {total} exercises </strong></p>
   )
 }
 
@@ -35,30 +35,57 @@ const Course = ({course}) => {
   
 }
 function App() {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />  
+      )}  
+    </>
   )
 }
 
