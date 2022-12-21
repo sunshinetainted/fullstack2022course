a5f8e3dce6de23a974e6d2c3baf2f28409c431b7
sunshinetainted <31520928+sunshinetainted@users.noreply.github.com>
2022-12-21 13:22:56 +0100

    Solution courseinfo step 6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,57 @@
+// import Course from './components/Course'
+const Header = ({course}) => 
+<h1>{course}</h1>
+
+const Part = ({part}) => 
+<p>
+  {part.name} {part.exercises}
+</p>
+
+const Content = ({parts, id}) => 
+<>
+  {parts.map(part => 
+    <Part key={part.id} part={part} />
+  )}
+</>
+
+// const Total = ({sum}) => <p>Number of exercises {sum}</p>
+
+const Course = ({course}) => {
+  return (
+    <>
+    <Header course={course.name}/>
+    <Content parts={course.parts} />      
+    {/* <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} /> */}
+    </>  
+  )
+  
+}
+function App() {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+export default App
---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,30 @@
+
+// const Header = ({course}) => 
+// <h1>{course}</h1>
+
+// const Part = ({part}) => 
+// <p>
+//   {part.name} {part.exercises}
+// </p>
+
+// const Content = ({parts, id}) => 
+// <>
+//   {parts.map(part => 
+//     <Part key={part.id} part={part} />
+//   )}
+// </>
+
+// // const Total = ({sum}) => <p>Number of exercises {sum}</p>
+
+// const Course = ({course}) => {
+//   return (
+//     <>
+//     <Header course={course.name}/>
+//     <Content parts={course.parts} />      
+//     {/* <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} /> */}
+//     </>  
+//   )
+  
+// }
+
+// export default Course
